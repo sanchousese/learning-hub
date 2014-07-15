06840c313871d3a979f6e2c2c78264df9227b565
sanchousese <saniasutula@gmail.com>
2014-07-16 00:14:57 +0400

    Student was added, and method for it

--- src/main/java/ua/com/learninghub/rest/Main.java
+++ src/main/java/ua/com/learninghub/rest/Main.java
@@ -1,15 +1,33 @@
 package ua.com.learninghub.rest;
+
+import org.codehaus.jettison.json.JSONObject;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/user/{userName}")
+@Path("/user")
 public class Main {
+
     @GET
+    @Path("/sayHello/{userName}")
     @Produces(MediaType.TEXT_PLAIN)
     public String sayPlainTextHello(@PathParam("userName") String name) {
         return "Hello, " + name + "!";
+    }
+
+    @GET
+    @Path("/getJson")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Student[] getJson() {
+        Student[] students = new Student[10];
+
+        for (int i = 0; i < students.length; i++) {
+            students[i] = new Student(i, i+1);
+        }
+
+        return students;
     }
 }
---
+++ src/main/java/ua/com/learninghub/rest/Student.java
@@ -0,0 +1,26 @@
+package ua.com.learninghub.rest;
+
+public class Student {
+    int x, y;
+
+    public Student(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
