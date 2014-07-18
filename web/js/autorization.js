d1d8112c35e6399219558350da88de9d9dc3d34d
sanchousese <saniasutula@gmail.com>
2014-07-18 14:29:25 +0400

    delete Main

---
+++ web/js/autorization.js
@@ -0,0 +1,26 @@
+      function submitButton() {
+        var Auth = {
+            login: $('#login').val(),
+            password: $('#password').val()
+        };
+        addData(Auth)
+    }
+
+    function addData(data){// pass your data in method
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:8080/rest/user/login",
+            data: JSON.stringify(data),
+            contentType: "application/json",
+            dataType: "json",
+            statusCode :{
+                200: function () {
+//                    alert("success...");
+                    document.location.href = "http://google.com";
+                },
+                401: function() {
+                    alert("Error");
+                 }
+            }
+        });
+    }
