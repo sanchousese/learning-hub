2d6746e6279036662ecee5eedd706d846ac76408
parkhomenko <dmitriy.parkhomenko@icloud.com>
2014-07-29 18:16:04 +0300

    Script Add

--- web/js/search.js
+++ web/js/search.js
@@ -1,43 +1,35 @@
-  function searchButton() {
+  function search_btn(){
           var keyWords = $('#searchField').val();
         alert(keyWords);
-//        returnCourses(keyWords);
-    }
-
-
-
-//function returnCourses(keyWords) {// pass your data in method
-//          $.ajax({
-//              type: "POST",
-//              url: "http://localhost:8080/rest/search/courses/byKeywords",  ///search/courses/byKeywords
-//              data: JSON.stringify(keyWords),
-//              contentType: "application/json; charset=utf-8",
-//              dataType: "json",
-//                  statusCode: {
-//                  200: function () {
-//                      $("#register-btn").hide('slow', function() {});
-//                      $("#hideRegisterForm").hide('slow', function() {});
-//                      $('#logout').css('display', 'block');
-//                      $('#login').click();
-//                      $('#user-info').css('display', 'block');
-//                      $.ajax({
-//                          //data: str,
-//                          type: "POST",
-//                          url: "http://localhost:8080/rest/user/userInfo",
-//                          datatype: "json",
-//                          contentType: "application/json",
-//                          success: function(data) {
-//                              document.getElementById("user-info").innerHTML = data.login;
-//
-//                          },
-//                          statusCode: {
-//                              403: function() {
-//                                  alert("Internal error");
-//                              }
-//                          }
-//                      });
-//                  },
-//                  401: function () {
-//                      alert("Invalid login or password");
-//                  }
-//              }
+        catalogCourses(keyWords);
+    }              
+              function catalogCourses(keyWords) {// pass your data in method
+                  alert("good");
+$.ajax({
+    //data: str,
+    type: "GET",
+//    url: "http://localhost:8080/rest/course/getAll",
+    url: "http://localhost:8080/rest/search/courses/byKeywords?keywords="+keyWords,
+    datatype: "json",
+    contentType: "application/json",
+    success: function(data) {
+        var div = document.getElementById("courseContainer");
+        for (var i = 0; i < data.length; i++) {
+            div.innerHTML +=
+                '<div class="col-xs-12 col-sm-6 col-md-6  placeholder padding_5" href="/Course.html">' +
+                '<div class="course_big">' +
+                '<div class="news_cover"><a href="javascript:openCourse(' + data[i].idCourse + ');">' +
+                    '<img id = "courseMainImage'+ i +'" src="../img/image.jpg" alt="Course img" class="img-rounded"></a></div>' +
+                '<div class="news_content">' +
+                '<h3><a href="javascript:openCourse(' + data[i].idCourse + ');">' + data[i].name + '</a></h3>' +
+                '<p>' + data[i].description + '</p>' +
+                '</div>' +
+                '</div>' +
+                '</div>';
+            }
+        for (var i = 0; i < data.length; i++) {
+            $("#courseMainImage"+i).attr('src','http://localhost:8080/rest/course/getLogoImage/'+(data[i].idCourse));
+        }
+        }
+    });
+}
