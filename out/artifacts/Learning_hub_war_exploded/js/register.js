1c469569c1c207a97655ef1c77e19a1907b72ebe
parkhomenko <dmitriy.parkhomenko@icloud.com>
2014-07-30 21:09:47 +0300

    Merge branch 'master' of /Users/parkhomenko/Documents/WEBSEO/Projects/learning-hub with conflicts.

--- out/artifacts/Learning_hub_war_exploded/js/register.js
+++ out/artifacts/Learning_hub_war_exploded/js/register.js
@@ -1,3 +1,30 @@
-		$("document").ready(function(){
-		    $('#modal').modal();
-		});
+//		$("document").ready(function(){
+//		    $('#modal').modal();
+//		});
+
+        function registerButton() {
+            var User = {
+                login: $('#inputName').val(),
+                pass: $('#inputPassword').val(),
+                email: $('#inputEmail').val()
+            };
+            addUser(User)
+        }
+
+        function addUser(data) {// pass your data in method
+            $.ajax({
+                type: "POST",
+                url: "http://localhost:8080/rest/user/addUser",
+                data: JSON.stringify(data),
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                statusCode: {
+                    200: function () {
+                        alert("Success...");
+                    },
+                    401: function () {
+                        alert("Invalid login or password");
+                    }
+                }
+            });
+        }
